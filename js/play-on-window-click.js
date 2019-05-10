981861c02129d106ea2aaf3c2a094fbbfb305487
WilKhalifa <43878318+WilKhalifa@users.noreply.github.com>
2019-05-10 12:15:00 +0200

    add event listener touchstart

--- js/play-on-window-click.js
+++ js/play-on-window-click.js
@@ -3,10 +3,10 @@ AFRAME.registerComponent('play-on-window-click', {
         this.onClick = this.onClick.bind(this);
     },
     play: function () {
-        window.addEventListener('click', this.onClick);
+        window.addEventListener('click touchstart', this.onClick);
     },
     pause: function () {
-        window.removeEventListener('click', this.onClick);
+        window.removeEventListener('click touchstart', this.onClick);
     },
     onClick: function (evt) {
         var video = this.el.components.material.material.map.image;
